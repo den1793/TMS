d16c26391df29bf60957bbeb908d0af4763d1e07
Denis Smirnov <den170193@yandex.ru>
2023-01-16 19:48:19 +0300

    refactor Operation.java

--- src/lesson8_homework/Operation.java
+++ src/lesson8_homework/Operation.java
@@ -17,18 +17,8 @@ public class Operation {
     }
 
     public double getNum1() {return num1;}
-
-    public void setNum1(double num1) { this.num1 = num1; }
-
     public double getNum2() { return num2; }
-
-    public void setNum2(double num2) { this.num2 = num2; }
-
     public String getType() { return type; }
-
-    public void setType(String type) { this.type = type; }
-
     public double getResult() { return result; }
-
     public void setResult(double result) { this.result = result; }
 }
